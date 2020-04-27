8a60157c56a015ff211b0e43fe7b04439af59dc7
BryantLan12138 <lubi980835699@gmail.com>
2020-04-27 21:58:34 +1000

    finished vpc setup

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -15,5 +15,117 @@ resource "aws_internet_gateway" "igw" {
 }
 
 
+resource "aws_default_route_table" "main" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "Assesment 2 for Tu Lan"
+  }
+}
+
+resource "aws_subnet" "public-az1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public AZ1"
+  }
+}
+
+
+resource "aws_subnet" "public-az2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public AZ2"
+  }
+}
+
+resource "aws_subnet" "public-az3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.8.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public AZ3"
+  }
+}
+
+
+resource "aws_subnet" "private-az1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.16.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "private AZ1"
+  }
+}
+
+resource "aws_subnet" "private-az2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.20.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "private AZ2"
+  }
+}
+
+resource "aws_subnet" "private-az3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.24.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "private AZ3"
+  }
+}
+
+resource "aws_subnet" "Data-az1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.32.0/22"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "data AZ1"
+  }
+}
+
+resource "aws_subnet" "Data-az2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.36.0/22"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "data AZ2"
+  }
+}
+
+resource "aws_subnet" "Data-az3" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.40.0/22"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "data AZ3"
+  }
+}
 
