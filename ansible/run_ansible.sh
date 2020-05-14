d0b8db84be63fdeb4c2956c603d8fb8ba47bd887
BryantLan12138 <lubi980835699@gmail.com>
2020-05-14 16:32:29 +1000

    deleted the keys

--- ansible/run_ansible.sh
+++ ansible/run_ansible.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set +ex 
-chmod 644 run_ansible.sh
 
 # todo genereate inventory.yml file with ec2 host
 echo "all:">inventory.yml
 echo "  hosts:">>inventory.yml
-echo "    \"$(cd ..&&terraform output instance_public_ip)\":">>inventory.yml
+echo "    \"$(cd ../infra && terraform output instance_public_ip)\":">>inventory.yml
 # todo add any additional variables
 
 
 # ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user --private-key ~/keys/ec2-key playbook.yml 
 
-ANSIBLE_HOST_KEY_CHECKING=False sudo ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user --private-key ec2-key --extra-vars "db_endpoint=$(cd ..&&terraform output db_endpoint)" playbook.yml
+ANSIBLE_HOST_KEY_CHECKING=False sudo ansible-playbook -i inventory.yml -e 'record_host_keys=True' -u ec2-user --private-key ~/.ssh/id_rsa --extra-vars "db_endpoint=$(cd ..&&terraform output db_endpoint)" playbook.yml
